4e659318a838304892755297550da5ea80b9cafe
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2023-08-11 07:57:11 -0500

    working on all supported file formats

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 // Attribution: spacedrive
 // https://github.com/spacedriveapp/spacedrive/tree/main/crates/file-ext
 pub mod extensions;
-// mod file;
 pub mod kind;
 pub mod magic;
 
+use crate::{extensions::Extension, magic::MagicBytes, magic::MagicBytesMeta};
 use home::home_dir;
 use nu_plugin::{serve_plugin, EvaluatedCall, LabeledError, MsgPackSerializer, Plugin};
-use nu_protocol::{Category, PluginExample, PluginSignature, Spanned, SyntaxShape, Value};
+use nu_protocol::{Category, PluginExample, PluginSignature, Span, Spanned, SyntaxShape, Value};
 use std::path::Path;
-
-use crate::{extensions::Extension, magic::MagicBytes};
-// use tokio::runtime::Handle;
 
 struct Implementation;
 
@@ -24,13 +21,33 @@ impl Implementation {
 impl Plugin for Implementation {
     fn signature(&self) -> Vec<PluginSignature> {
         vec![PluginSignature::build("file")
-            .usage("View file results")
-            .required("path", SyntaxShape::String, "path to file input file")
+            .usage("View file format information")
+            .required(
+                "filename",
+                SyntaxShape::String,
+                "full path to file name to inspect",
+            )
             .category(Category::Experimental)
             .plugin_examples(vec![PluginExample {
-                description: "This is the example descripion".into(),
-                example: "some pipeline involving file".into(),
-                result: None,
+                description: "Get format information from file".into(),
+                example: "file some.jpg".into(),
+                result: Some(Value::Record {
+                    cols: vec![
+                        "description".into(),
+                        "format".into(),
+                        "magic_offset".into(),
+                        "magic_length".into(),
+                        "magic_bytes".into(),
+                    ],
+                    vals: vec![
+                        Value::string("Image", Span::test_data()),
+                        Value::string("jpg", Span::test_data()),
+                        Value::string("0", Span::test_data()),
+                        Value::string("2", Span::test_data()),
+                        Value::string("[FF, D8]", Span::test_data()),
+                    ],
+                    span: Span::test_data(),
+                }),
             }])]
     }
 
@@ -42,7 +59,6 @@ impl Plugin for Implementation {
     ) -> Result<Value, LabeledError> {
         assert_eq!(name, "file");
         let param: Option<Spanned<String>> = call.opt(0)?;
-        // eprintln!("starting run");
 
         if let Some(filename) = param {
             let home_dir = match home_dir() {
@@ -61,96 +77,197 @@ impl Plugin for Implementation {
             } else {
                 filename.item
             };
-            // eprintln!("filename: {:?}", &filename);
             let canon_path = Path::new(&filename).canonicalize().unwrap();
-            // eprintln!("canon_path: {:?}", &canon_path);
             let file_format = extensions::Extension::resolve_conflicting(canon_path, true);
-            // eprintln!("file_format: {:?}", file_format);
             return match file_format {
-                Some(file_format) => {
-                    match file_format {
-                        Extension::Document(document_format) => {
-                            return Ok(Value::string(document_format.to_string(), span))
-                        }
-                        Extension::Video(video_format) => {
-                            return Ok(Value::string(video_format.to_string(), span))
-                        }
-                        Extension::Image(image_format) => {
-                            // let f = image_format[jpg];
-                            let magic = image_format.magic_bytes_meta();
-                            // return Ok(Value::string(
-                            //     format!("Image Format: {image_format} Bytes: {bytes:?}"),
-                            //     span,
-                            // ));
-                            let offsets = magic
-                                .iter()
-                                .map(|b| b.offset.to_string())
-                                .collect::<Vec<_>>();
-                            let lengths = magic
-                                .iter()
-                                .map(|b| b.length.to_string())
-                                .collect::<Vec<_>>();
-                            let mbytes = magic
-                                .iter()
-                                .map(|b| format!("{:X?}", b.bytes.clone()))
-                                .collect::<Vec<_>>();
-                            let cols = vec![
-                                "description".into(),
-                                "format".into(),
-                                "magic_offset".into(),
-                                "magic_length".into(),
-                                "magic_bytes".into(),
-                            ];
-                            let vals = vec![
-                                Value::string("Image", span),
-                                Value::string(format!("{image_format}"), span),
-                                Value::string(offsets.join(", "), span),
-                                Value::string(lengths.join(", "), span),
-                                Value::string(format!("{}", mbytes.join(", ")), span),
-                            ];
-                            return Ok(Value::Record { cols, vals, span });
-                        }
-                        Extension::Audio(audio_format) => {
-                            return Ok(Value::string(audio_format.to_string(), span))
-                        }
-                        Extension::Archive(archive_format) => {
-                            return Ok(Value::string(archive_format.to_string(), span))
-                        }
-                        Extension::Executable(executable_format) => {
-                            return Ok(Value::string(executable_format.to_string(), span))
-                        }
-                        Extension::Text(text_format) => {
-                            return Ok(Value::string(text_format.to_string(), span))
-                        }
-                        Extension::Encrypted(encrypted_format) => {
-                            return Ok(Value::string(encrypted_format.to_string(), span))
-                        }
-                        Extension::Key(key_format) => {
-                            return Ok(Value::string(key_format.to_string(), span))
-                        }
-                        Extension::Font(font_format) => {
-                            return Ok(Value::string(font_format.to_string(), span))
-                        }
-                        Extension::Mesh(mesh_format) => {
-                            return Ok(Value::string(mesh_format.to_string(), span))
-                        }
-                        Extension::Code(code_format) => {
-                            return Ok(Value::string(code_format.to_string(), span))
-                        }
-                        Extension::Database(database_format) => {
-                            return Ok(Value::string(database_format.to_string(), span))
-                        }
-                        Extension::Book(book_format) => {
-                            return Ok(Value::string(book_format.to_string(), span))
-                        }
+                Some(file_format) => match file_format {
+                    Extension::Document(document_format) => {
+                        let magic = document_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Document",
+                            document_format.to_string(),
+                            span,
+                        ));
                     }
-                    // Ok(Value::string(file_format.to_string(), span))
-                }
+                    Extension::Video(video_format) => {
+                        let magic = video_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Video",
+                            video_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Image(image_format) => {
+                        let magic = image_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Image",
+                            image_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Audio(audio_format) => {
+                        let magic = audio_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Audio",
+                            audio_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Archive(archive_format) => {
+                        let magic = archive_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Archive",
+                            archive_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Executable(executable_format) => {
+                        let magic = executable_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Executable",
+                            executable_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Text(text_format) => {
+                        return Ok(get_text_format_details(
+                            "Text",
+                            text_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Encrypted(encrypted_format) => {
+                        let magic = encrypted_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Encrypted",
+                            encrypted_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Key(key_format) => {
+                        return Ok(get_text_format_details("Key", key_format.to_string(), span));
+                    }
+                    Extension::Font(font_format) => {
+                        let magic = font_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Font",
+                            font_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Mesh(mesh_format) => {
+                        let magic = mesh_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Mesh",
+                            mesh_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Code(code_format) => {
+                        return Ok(get_text_format_details(
+                            "Code",
+                            code_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Database(database_format) => {
+                        let magic = database_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Database",
+                            database_format.to_string(),
+                            span,
+                        ));
+                    }
+                    Extension::Book(book_format) => {
+                        let magic = book_format.magic_bytes_meta();
+                        return Ok(get_magic_details(
+                            magic,
+                            "Book",
+                            book_format.to_string(),
+                            span,
+                        ));
+                    }
+                },
                 None => Ok(Value::Nothing { span: call.head }),
             };
         }
 
         Ok(Value::Nothing { span: call.head })
+    }
+}
+
+fn get_magic_details(
+    magic: Vec<MagicBytesMeta>,
+    format: &str,
+    data_format: String,
+    span: Span,
+) -> Value {
+    // let mut details = vec![];
+    // details.push(Value::string(format, magic.span));
+    // details.push(Value::string(magic.format, magic.span));
+    // details.push(Value::string(magic.offset.to_string(), magic.span));
+    // details.push(Value::string(magic.length.to_string(), magic.span));
+    // details.push(Value::string(format!("{:X?}", magic.bytes), magic.span));
+    // details
+    let offsets = magic
+        .iter()
+        .map(|b| b.offset.to_string())
+        .collect::<Vec<_>>();
+    let lengths = magic
+        .iter()
+        .map(|b| b.length.to_string())
+        .collect::<Vec<_>>();
+    let mbytes = magic
+        .iter()
+        .map(|b| format!("{:X?}", b.bytes.clone()))
+        .collect::<Vec<_>>();
+    let cols = vec![
+        "description".into(),
+        "format".into(),
+        "magic_offset".into(),
+        "magic_length".into(),
+        "magic_bytes".into(),
+    ];
+    let vals = vec![
+        Value::string(format, span),
+        Value::string(data_format, span),
+        Value::string(offsets.join(", "), span),
+        Value::string(lengths.join(", "), span),
+        Value::string(format!("{}", mbytes.join(", ")), span),
+    ];
+
+    Value::Record { cols, vals, span }
+}
+
+fn get_text_format_details(format: &str, text_format: String, span: Span) -> Value {
+    let cols = vec![
+        "description".into(),
+        "format".into(),
+        "magic_offset".into(),
+        "magic_length".into(),
+        "magic_bytes".into(),
+    ];
+
+    Value::Record {
+        cols,
+        vals: vec![
+            Value::string(format, span),
+            Value::string(text_format, span),
+            Value::nothing(span),
+            Value::nothing(span),
+            Value::nothing(span),
+        ],
+        span,
     }
 }
 
