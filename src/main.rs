969223eac6efdcdd4ba166de98a63f2e48f39942
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2024-06-21 06:37:37 -0500

    update to latest nushell version

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,10 @@ use std::path::Path;
 struct FilePlugin;
 
 impl Plugin for FilePlugin {
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
+
     fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
         vec![Box::new(Implementation)]
     }
